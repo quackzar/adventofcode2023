7f89b0c0c771700a86f77c87ed12f5aae8f78d84
Mikkel Wienberg Madsen <mikkel.wienberg@alexandra.dk>
2023-12-04 15:58:28 +0100

    Start on day4

---
+++ day4/src/main.rs
@@ -0,0 +1,26 @@
+const INPUT: &str = "\
+Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 1
+";
+
+
+fn main() {
+    for line in INPUT.lines() {
+        let (card, numbers) = line.split_once(':').unwrap();
+        let (_, num) = card.split_once(' ').unwrap();
+        println!("Got {num}");
+
+        let (winning, holding) = numbers.split_once('|').unwrap();
+        dbg!(&winning);
+        dbg!(&holding);
+
+        let winning : Vec<u8> = winning.split(' ').filter_map(|num| num.parse().ok()).collect();
+        let holding : Vec<u8> = holding.split(' ').filter_map(|num| num.parse().ok()).collect();
+        dbg!(winning);
+        dbg!(holding);
+    }
+}
